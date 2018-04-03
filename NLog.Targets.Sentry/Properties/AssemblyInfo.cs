5a3cd3c067d0ca3c8ebd9bd419b22de0e1cbfbcf
Rolf Kristensen <sweaty1@hotmail.com>
2018-04-03 23:38:59 +0200

    NetStandard and VS2017

--- NLog.Targets.Sentry/Properties/AssemblyInfo.cs
+++ NLog.Targets.Sentry/Properties/AssemblyInfo.cs
@@ -2,17 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NLog.Targets.Sentry2")]
-[assembly: AssemblyDescription("Custom target for NLog enabling you to send logging messages to the Sentry logging service.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Bjarne Riis, Dillon Buchanan")]
-[assembly: AssemblyProduct("NLog.Targets.Sentry2")]
-[assembly: AssemblyCopyright("Copyright © 2016 Bjarne Riis")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e14fa010-29e4-48a5-9268-a26215202a72")]
-[assembly: AssemblyVersion("1.3.6")]
-[assembly: AssemblyFileVersion("1.3.6")]
-[assembly: AssemblyInformationalVersion("1.3.6")]
 [assembly: InternalsVisibleTo("NLog.Targets.Sentry.UnitTests")]
